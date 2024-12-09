02f806d324661899fd00c1467103f4bc307cb22d
Vladimir <jaba0192837465@gmail.com>
2024-12-09 21:06:01 +0300

    first commit

---
+++ src/flags/flags.c
@@ -0,0 +1,75 @@
+#include <string.h>
+#include <getopt.h>
+
+#include "flags/flags.h"
+#include "logger/liblogger.h"
+#include "utils/utils.h"
+
+#define CASE_ENUM_TO_STRING_(error) case error: return #error
+const char* flags_strerror(const enum FlagsError error)
+{
+    switch(error)
+    {
+        CASE_ENUM_TO_STRING_(FLAGS_ERROR_SUCCESS);
+        CASE_ENUM_TO_STRING_(FLAGS_ERROR_FAILURE);
+        default:
+            return "UNKNOWN_FLAGS_ERROR";
+    }
+    return "UNKNOWN_FLAGS_ERROR";
+}
+#undef CASE_ENUM_TO_STRING_
+
+
+enum FlagsError flags_objs_ctor(flags_objs_t* const flags_objs)
+{
+    lassert(!is_invalid_ptr(flags_objs), "");
+
+    if (!strncpy(flags_objs->log_folder, "./log/", FILENAME_MAX))
+    {
+        perror("Can't strncpy flags_objs->log_folder");
+        return FLAGS_ERROR_SUCCESS;
+    }
+
+    return FLAGS_ERROR_SUCCESS;
+}
+
+enum FlagsError flags_objs_dtor (flags_objs_t* const flags_objs)
+{
+    lassert(!is_invalid_ptr(flags_objs), "");
+
+    return FLAGS_ERROR_SUCCESS;
+}
+
+enum FlagsError flags_processing(flags_objs_t* const flags_objs, 
+                                 const int argc, char* const argv[])
+{
+    lassert(!is_invalid_ptr(flags_objs), "");
+    lassert(!is_invalid_ptr(argv), "");
+    lassert(argc, "");
+
+    int getopt_rez = 0;
+    while ((getopt_rez = getopt(argc, argv, "l:")) != -1)
+    {
+        switch (getopt_rez)
+        {
+            case 'l':
+            {
+                if (!strncpy(flags_objs->log_folder, optarg, FILENAME_MAX))
+                {
+                    perror("Can't strncpy flags_objs->log_folder");
+                    return FLAGS_ERROR_FAILURE;
+                }
+
+                break;
+            }
+
+            default:
+            {
+                fprintf(stderr, "Getopt error - d: %d, c: %c\n", getopt_rez, (char)getopt_rez);
+                return FLAGS_ERROR_FAILURE;
+            }
+        }
+    }
+    
+    return FLAGS_ERROR_SUCCESS;
+}
---
+++ src/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <locale.h>
+
+#include "logger/liblogger.h"
+#include "flags/flags.h"
+
+int init_all(flags_objs_t* const flags_objs, const int argc, char* const * argv);
+int dtor_all(flags_objs_t* const flags_objs);
+
+int main(const int argc, char* const argv[])
+{
+    flags_objs_t flags_objs = {};
+
+    if (init_all(&flags_objs, argc, argv))
+    {
+        fprintf(stderr, "Can't init all\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("Hello world!\n");
+    
+    if (dtor_all(&flags_objs))
+    {
+        fprintf(stderr, "Can't dtor all\n");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int logger_init(char* const log_folder);
+
+int init_all(flags_objs_t* const flags_objs, const int argc, char* const * argv)
+{
+    lassert(argc, "");
+    lassert(argv, "");
+
+    if (!setlocale(LC_ALL, "ru_RU.utf8"))
+    {
+        fprintf(stderr, "Can't setlocale\n");
+        return EXIT_FAILURE;
+    }
+
+
+    FLAGS_ERROR_HANDLE(flags_objs_ctor (flags_objs));
+    FLAGS_ERROR_HANDLE(flags_processing(flags_objs, argc, argv));
+
+    if (logger_init(flags_objs->log_folder))
+    {
+        fprintf(stderr, "Can't logger init\n");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int dtor_all(flags_objs_t* const flags_objs)
+{
+    LOGG_ERROR_HANDLE(                                                               logger_dtor());
+    FLAGS_ERROR_HANDLE(                                                flags_objs_dtor(flags_objs));
+
+    return EXIT_SUCCESS;
+}
+
+#define LOGOUT_FILENAME "logout.log"
+// #define   DUMB_FILENAME "dumb"
+int logger_init(char* const log_folder)
+{
+    lassert(log_folder, "");
+
+    char logout_filename[FILENAME_MAX] = {};
+    if (snprintf(logout_filename, FILENAME_MAX, "%s%s", log_folder, LOGOUT_FILENAME) <= 0)
+    {
+        perror("Can't snprintf logout_filename");
+        return EXIT_FAILURE;
+    }
+
+    // char dumb_filename[FILENAME_MAX] = {};
+    // if (snprintf(dumb_filename, FILENAME_MAX, "%s%s", log_folder, DUMB_FILENAME) <= 0)
+    // {
+    //     perror("Can't snprintf dumb_filename");
+    //     return EXIT_FAILURE;
+    // }
+
+    LOGG_ERROR_HANDLE(logger_ctor());
+    LOGG_ERROR_HANDLE(logger_set_level_details(LOG_LEVEL_DETAILS_ALL));
+    LOGG_ERROR_HANDLE(logger_set_logout_file(logout_filename));
+
+    // TREE_DUMB_ERROR_HANDLE(tree_dumb_ctor());
+    // TREE_DUMB_ERROR_HANDLE(tree_dumb_set_out_file(dumb_filename));
+    
+    return EXIT_SUCCESS;
+}
+#undef LOGOUT_FILENAME
+// #undef   DUMB_FILENAME
